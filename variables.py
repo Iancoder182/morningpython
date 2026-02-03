650a1d12066c347e539181767a4ae04c5a0cdcfc
Iancoder182 <mwendaian182@gmail.com>
2026-02-03 10:25:50 +0300

    initial commit

---
+++ variables.py
@@ -0,0 +1,17 @@
+#Variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "Ian"
+lastname = "Prince"
+num1 = 24
+num2 = 42
+language = "Python"
+
+print(school)
+print(firstname,lastname)
+print(num1+num2)
+
+print(language,"is a web programming language")
+
+#The sum of 24 and 42 is 66
+print("The sum of", num1, "and", num2, "is 66" )
